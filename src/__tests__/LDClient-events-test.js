e639fde3b2fe22054edf8216a0d24c14d9b222b0
LaunchDarklyCI <LaunchDarklyCI@users.noreply.github.com>
2020-02-14 13:20:25 -0800

    prepare 1.4.0 release (#9)

--- src/__tests__/LDClient-events-test.js
+++ src/__tests__/LDClient-events-test.js
@@ -1,5 +1,7 @@
 const LDClient = require('../index');
 
+const { TestHttpHandlers, TestHttpServers, withCloseable } = require('launchdarkly-js-test-helpers');
+
 describe('LDClient', () => {
   const envName = 'UNKNOWN_ENVIRONMENT_ID';
   const user = { key: 'user' };
@@ -9,33 +11,55 @@ describe('LDClient', () => {
   // should.
 
   describe('event generation', () => {
-    function stubEventProcessor() {
-      const ep = { events: [] };
-      ep.start = function() {};
-      ep.flush = function() {};
-      ep.stop = function() {};
-      ep.enqueue = function(e) {
-        ep.events.push(e);
-      };
-      return ep;
-    }
-
     // This tests that the client calls our platform's getCurrentUrl() and isDoNotTrack() methods.
-    it('sends an event for track()', done => {
-      const ep = stubEventProcessor();
-      const client = LDClient.initialize(envName, user, { eventProcessor: ep, bootstrap: {} });
-      const data = { thing: 'stuff' };
-      client.on('ready', () => {
-        client.track('eventkey', data);
+    it('sends an event for track()', async () => {
+      await withCloseable(TestHttpServers.start, async server => {
+        const config = { bootstrap: {}, eventsUrl: server.url, diagnosticOptOut: true };
+        const client = LDClient.initialize(envName, user, config);
+        await withCloseable(client, async () => {
+          const data = { thing: 'stuff' };
+          await client.waitForInitialization();
 
-        expect(ep.events.length).toEqual(2);
-        const trackEvent = ep.events[1];
-        expect(trackEvent.kind).toEqual('custom');
-        expect(trackEvent.key).toEqual('eventkey');
-        expect(trackEvent.user).toEqual(user);
-        expect(trackEvent.data).toEqual(data);
-        expect(trackEvent.url).toEqual(null);
-        done();
+          client.track('eventkey', data);
+          await client.flush();
+
+          const req = await server.nextRequest();
+          expect(req.path).toEqual('/events/bulk/' + envName);
+          const events = JSON.parse(req.body);
+          expect(events.length).toEqual(2); // first is identify event
+          const trackEvent = events[1];
+          expect(trackEvent.kind).toEqual('custom');
+          expect(trackEvent.key).toEqual('eventkey');
+          expect(trackEvent.userKey).toEqual(user.key);
+          expect(trackEvent.data).toEqual(data);
+          expect(trackEvent.url).toEqual(null);
+        });
+      });
+    });
+  });
+
+  describe('diagnostic events', () => {
+    it('sends diagnostic init event', async () => {
+      await withCloseable(TestHttpServers.start, async server => {
+        server.byDefault(TestHttpHandlers.respond(202));
+        const config = { bootstrap: {}, eventsUrl: server.url };
+        const client = LDClient.initialize(envName, user, config);
+        await withCloseable(client, async () => {
+          const req0 = await server.nextRequest();
+          const req1 = await server.nextRequest();
+          // We should have received both the client's usual identify event and a diagnostic init event, but we can't be sure of the ordering.
+          const expectedPath = '/events/diagnostic/' + envName;
+          expect([req0.path, req1.path]).toContain(expectedPath);
+          const req = req0.path === expectedPath ? req0 : req1;
+          const data = JSON.parse(req.body);
+          expect(data.kind).toEqual('diagnostic-init');
+          expect(data.platform).toMatchObject({
+            name: 'Node',
+          });
+          expect(data.sdk).toMatchObject({
+            name: 'node-client-sdk',
+          });
+        });
       });
     });
   });
