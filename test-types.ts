e4cc16dcfa8e15795bdee46890638a12a16995d1
Eli Bishop <eli@launchdarkly.com>
2021-04-01 12:32:16 -0700

    add inlineUsersToEvents to TypeScript defs by updating js-sdk-common

--- test-types.ts
+++ test-types.ts
@@ -18,6 +18,7 @@ var allOptions: ld.LDOptions = {
   sendEvents: true,
   allAttributesPrivate: true,
   privateAttributeNames: [ 'x' ],
+  inlineUsersInEvents: true,
   allowFrequentDuplicateEvents: true,
   sendEventsOnlyForVariation: true,
   flushInterval: 1,
